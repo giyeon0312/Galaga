80bc8cb30824b834a5fd7535f49d390437fa098a
giyeon0312 <giyeon0312@gmail.com>
2022-12-11 00:44:16 +0900

    [Add] 스테이지 밖으로 넘어가면 삭제 처리

--- Galaga/ActorManager.cpp
+++ Galaga/ActorManager.cpp
@@ -22,10 +22,23 @@ void CActorManager::Init()
 void CActorManager::Update()
 {
 	int cnt = m_pActors.size();
-	for (int i = 0; i < cnt; i++)
+
+	for (vector<CActor*>::iterator iter = m_pActors.begin(); iter != m_pActors.end();)
 	{
-		CActor* actor = m_pActors[i];
+		CActor* actor = *iter;
 		actor->Update();
+
+		// 테두리 밖으로 나가면 삭제하기.
+		POSITION pos = actor->GetPosition();
+		if (pos.y <= 0 || pos.y >= STAGE_HEIGHT || pos.x <= 0 || pos.x >= STAGE_WIDTH)
+		{
+			// 벡터에서 지우고
+			SAFE_DELETE(actor);
+			iter = m_pActors.erase(iter); // 다음 iterator 반환한다.
+			// 삭제 됐을 경우 다음 iter를 여기서 받아가기 때문에 iter++를 하면 다음을 건너뛰게 된다.
+		}
+		else
+			iter++;
 	}
 }
 
